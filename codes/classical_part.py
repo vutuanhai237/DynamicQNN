04a9ba3e9c417aea604283cc9291dc5e243dc2a4
Vũ Tuấn Hải <haimeohung@gmail.com>
2022-07-28 17:02:12 +0700

    Run 50 filter MNIST Fashion

--- codes/classical_part.py
+++ codes/classical_part.py
@@ -142,7 +142,7 @@ def load_mnist(n_train: int, n_val: int, n_test: int, quanv: types.FunctionType 
         
         return x_train, y_train, x_val, y_val, x_test, y_test
 
-def load_mnist_fashion(n_train: int, n_val: int, n_test: int, quanv: types.FunctionType = quantum_model):
+def load_mnist_fashion(n_train: int, n_val: int, n_test: int, quanv: types.FunctionType = quantum_model, is_take_xq = False):
     """_summary_
 
     Args:
@@ -176,8 +176,11 @@ def load_mnist_fashion(n_train: int, n_val: int, n_test: int, quanv: types.Funct
     y_val = np_utils.to_categorical(y_val, 10)
     y_test = np_utils.to_categorical(y_test, 10)
     # Create post-processing data (the data that has gone through the quanvolutional layer)
-    xq_train = converter(x_train, quanv)
-    xq_val = converter(x_val, quanv)
-    xq_test = converter(x_test, quanv)
+    if is_take_xq:
+        xq_train = converter(x_train, quanv)
+        xq_val = converter(x_val, quanv)
+        xq_test = converter(x_test, quanv)
 
-    return x_train, xq_train, y_train, x_val, xq_val, y_val, x_test, xq_test, y_test
+        return x_train, xq_train, y_train, x_val, xq_val, y_val, x_test, xq_test, y_test
+    else:
+        return x_train, y_train, x_val, y_val, x_test, y_test
